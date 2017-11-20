7440bc0fec37ff1ec201b501a12174160dcb8eaa
Sriharsha <sganti@levi.com>
2017-11-20 15:44:28 -0800

    Enabled eureka discovery

--- cart-service/src/main/java/com/microecom/cart/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/microecom/cart/cartservice/CartServiceApplication.java
@@ -3,11 +3,7 @@ package com.microecom.cart.cartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CartServiceApplication {
