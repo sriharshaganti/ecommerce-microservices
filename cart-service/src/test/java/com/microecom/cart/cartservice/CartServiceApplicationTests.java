8f88c9531af942ae36c6d9416eee2a9fb32a7eff
Sriharsha <sganti@levi.com>
2017-11-20 13:29:17 -0800

    Added cart service

---
+++ cart-service/src/test/java/com/microecom/cart/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microecom.cart.cartservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CartServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
